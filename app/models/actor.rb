3b9d65c2db9132f49358ca8046018b258b3a11d5
Brian Rosengarten <barosengarten@gmail.com>
2019-01-30 03:30:22 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,8 +8,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.collect do |c|
-    "#{c.name} - #{c.show.name}"
+    # self.characters.collect do |c|
+    # "#{c.name} - #{c.show.name}"
   end
 
 end
